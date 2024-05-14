e6efceeca5495f12db8905235445c8406d14f5df
twschool <98859425+twschool@users.noreply.github.com>
2024-05-15 10:31:12 +1200

    attempting to add smooth background screen wrapping

--- spawn_sprites_v3.py
+++ spawn_sprites_v3.py
@@ -18,16 +18,31 @@ class Coordinate:
         self.y = y
     
     def format(self):
+        """Returns the players formatted coordinates (x, y) as a tuple"""
         return (self.x, self.y)
 
 
-class Player:
+class Player(Coordinate):
     def __init__(self, color):
+        # Initiate the coordinate class
+        super().__init__(100, 100)
+        
+        
         self.color = color
-        self.image = pygame.image.load(get_player_filename(color))
+        image = pygame.image.load(get_player_filename(color))
+        
+        # Scale the player 
+        scale = 1.25
+
+        self.image = pygame.transform.scale(image, (int(image.get_width() * scale)
+                                                         , int(image.get_height() * scale)))
 
     def get_player_filename(self):
         return PLAYER_PREFIX + self.color + IMAGE_SUFFIX
+
+    def get_coords(self):
+        """Return the x, y coords as a tuple (x, y)"""
+        return self.format()
 
 
 def background():
@@ -46,13 +61,12 @@ def background():
         background_coords[1].y += 5
         
         
-        if background_coords[0].y >= 700:
-            background_coords[0].y = -700
-            background_coords[1].y = 0
+        if background_coords[0].y >= y_lower_bound:
+            background_coords[0].y = y_upper_bound
         
-        if background_coords[1].y >= 700:
-            background_coords[1].y = -700
-            background_coords[0].y = 0
+        if background_coords[1].y >= y_lower_bound:
+            background_coords[1].y = y_upper_bound
+
     
     move_background()
     
@@ -60,10 +74,10 @@ def background():
     screen.blit(background_image, background_coords[1].format())
     
 
-def player():
+def player(player_object: Player):
     """Draw the player image on the screen"""
     
-    screen.blit(player_image_global, player_coords.format())
+    screen.blit(player_object.image, player_object.get_coords())
 
 
 # Initialize pygame and set the display caption
@@ -75,23 +89,38 @@ screen = pygame.display.set_mode(DISPLAY_SIZE)
 player_velocity = 0
 velocity = Coordinate(0, 0) # Store the players velocity as a coordinate x,y
 player_coords = Coordinate(100, 100)
-background_coords: list[Coordinate] = [Coordinate(0, 0), Coordinate(0, -700)]
 
+# Define all background related variables
+background_coords: list[Coordinate] = [Coordinate(0, 0), Coordinate(0, -700)]
+background_image = pygame.image.load("background.png")
+background_scale_x =  DISPLAY_SIZE[0] / background_image.get_width()
+background_scale_y =  (DISPLAY_SIZE[1] * 2) / background_image.get_height()
+background_image = pygame.transform.scale(background_image, (int(background_image.get_width() * background_scale_x)
+                                                         , int(background_image.get_height() * background_scale_y)))
+
+new_background_image_height = background_image.get_height()
+fps = 60
+
+
+# Defines the y upper and lower bound to give the illusion of screen wrapping for the background
+y_upper_bound = -new_background_image_height
+y_lower_bound = DISPLAY_SIZE[1]
 
 # Get the players sprite based on the color given
 player_images = ["black", "blue", "red", "white"]
 get_player_filename = lambda player_color: PLAYER_PREFIX + player_color + IMAGE_SUFFIX
 
 player_image_global = pygame.image.load( get_player_filename("red") )
-background_image = pygame.image.load("background.png")
-background_image = pygame.transform.scale(background_image, DISPLAY_SIZE)
 
-fps = 60
+
+
 
 
 # Main routine
 
 finished = False
+player_object = Player("blue")
+
 
 # Main loop
 while not finished:
@@ -99,7 +128,7 @@ while not finished:
     clock = pygame.time.Clock()
     screen.fill((0, 0, 0))
     background()
-    player()
+    player(player_object)
     
     pygame.display.update()
     clock.tick(fps)
@@ -113,9 +142,9 @@ while not finished:
         elif event.type == pygame.KEYDOWN:
             # Player movement (horizontal only for now)
             if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                player_coords.x -= 20
+                player_object.x -= 20
             elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                player_coords.x += 20
+                player_object.x += 20
 
             
         elif event.type == pygame.MOUSEBUTTONDOWN:
